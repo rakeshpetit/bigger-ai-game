1b893ca87a2bad886372d7ebf54e889b857745d4
Rakesh Arunachalam <rakesh@thoughtbot.com>
2025-05-30 10:18:52 +0100

    Add support for Netlify deployment

--- app.config.ts
+++ app.config.ts
@@ -1,19 +1,22 @@
-import { defineConfig } from '@tanstack/react-start/config'
-import tsConfigPaths from 'vite-tsconfig-paths'
+import { defineConfig } from "@tanstack/react-start/config";
+import tsConfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   tsr: {
-    appDirectory: 'src',
+    appDirectory: "src",
   },
   vite: {
     ssr: {
-      noExternal: ['@mui/*'],
+      noExternal: ["@mui/*"],
     },
 
     plugins: [
       tsConfigPaths({
-        projects: ['./tsconfig.json'],
+        projects: ["./tsconfig.json"],
       }),
     ],
   },
-})
+  server: {
+    preset: "netlify",
+  },
+});
